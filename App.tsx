ac1a0ffb99e412f4d0de1fb58a9f6c1622e57aaa
Alypher Mansano Gomes <alypher@gmail.com>
2022-03-08 21:48:58 -0300

    fist commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import moment from "moment"
 
 export default function App() {
+  const [state, setState] = useState({
+    days: 0,
+    hours: 0,
+    mins: 0,
+    secs: 0,
+    date:  moment.duration().add({
+      days:0,
+      hours:0,
+      minutes:0,
+      seconds:10
+    }),
+    status: 'inProgress' 
+  })
+
+  const countdown = () => {
+    const timer = setInterval(() => {
+      setState(state => {
+        let { date } = state
+
+        if(date.valueOf() <= 0){
+          clearInterval(timer)
+          alert('Message sent!')
+          return {...state, status: 'done'}
+        } else {
+          date = date.subtract(1,"s")
+          
+          const days = date.days()
+          const hours = date.hours()
+          const mins = date.minutes()
+          const secs = date.seconds()
+    
+          return({
+            days,
+            hours,
+            mins,
+            secs,
+            date
+          })
+        }
+      })
+    },1000)
+  }
+
+  useEffect(() => {
+    countdown()
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>SECURITY GUARANTEE</Text>
+      <Text>{`${state?.days} : ${state?.hours} : ${state?.mins} : ${state?.secs}`}</Text>
+
+      <View id="config" style={styles.configMenu}>
+        <Button title="CFG" onPress={() => alert('Config')} />
+      </View>
     </View>
   );
 }
@@ -17,4 +69,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  configMenu: {
+    display: 'flex',
+    position: 'absolute',
+    bottom: 20,
+    left: 20
+  },
+  sendButton: {
+    display: 'flex',
+    position: 'absolute',
+    bottom: 20,
+    right: 20
+  }
 });
